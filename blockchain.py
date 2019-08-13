af6f65a6ebf72b4b42b9ca9a3b36121a7addf257
PEC0562 <50139725+PEC0562@users.noreply.github.com>
2019-08-14 02:01:07 +0800

    Add files via upload

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,5 @@
 import time
 import hashlib
-import json
 import string
 import random
 
@@ -22,7 +21,7 @@ class Block():
         self.time_for_cal=time.localtime(time.time())
         self.nonce = 1
         self.hash = self.get_hash()
-
+#將交易訊息放入merkle tree 並算出此block的hash
     def get_hash(self):
         self.merkle_tree.append("")
         hash256_1 = hashlib.sha256()
@@ -41,6 +40,7 @@ class Block():
         hash256_14 = hashlib.sha256()
         hash256_15 = hashlib.sha256()
         hash256_16 = hashlib.sha256()
+        hash256_17 = hashlib.sha256()
         hash256_1.update(self.msg1.encode('gb2312'))
         hash256_2.update(self.msg2.encode('gb2312'))
         hash256_3.update(self.msg3.encode('gb2312'))
@@ -73,8 +73,9 @@ class Block():
         self.merkle_tree.append(hash256_8.hexdigest())
         data = self.time_stamp + self.merkle_tree[1] + str(self.nonce) + self.previous_hash
         hash256_16.update(data.encode('gb2312'))
-        return hash256_16.hexdigest()
-
+        hash256_17.update((hash256_16.hexdigest()).encode('gb2312'))
+        return hash256_17.hexdigest()
+#挖礦函式
     def mine(self, diffculty):
         target = ''
 
@@ -101,8 +102,6 @@ class EduChain():
         self.list.append(Block)
 
     def show(self,block_number):
-        #json_res = json.dumps(self.list, default=self.block_dict, sort_keys = True, indent = 4, separators=(',', ': '))
-        #print(json_res)
         print("Block ", block_number, ":")
         print("{")
         print(" message 1: ", self.list[block_number].msg1)
@@ -171,23 +170,16 @@ class EduChain():
         else:
             print(self.message[number] , " this message is correct, and it is located in Block ",block_number)
 
-    def message_generator(self):
-        chars = string.ascii_uppercase + string.digits
-        word = ''.join(random.choice(chars) for _ in range(6))
-        self.message.append(word)
+    #def message_generator(self):
+    #    chars = string.ascii_uppercase + string.digits
+    #    word = ''.join(random.choice(chars) for _ in range(6))
+    #    self.message.append
 
-   # def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
-    #    return ''.join(random.choice(chars) for _ in range(size))
-
-
-
-print('EduChain CLT Apr 2018 by fxt0706')
 c = EduChain(4)
 c.add_block(Block("first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", '0'))
 message_length=0
 check_block_length=0
 check_message_length=0
-#line_pointer=0
 j=0
 while(1):
     with open('message.txt', mode='r') as file:
@@ -195,23 +187,14 @@ while(1):
        file.seek(0)
        if(message_length!=len(lines)):
             if (len(lines) % 8 == 0):
-
-                #file.seek(,message_length)
                 for line in file.readlines()[message_length:len(lines)]:
-                    # print(line)
                     line = line.strip('\n')
                     c.message.append(str(line))
-                 # 讀取交易訊息
                 c.add_block(Block(c.message[8 * j], c.message[8 * j + 1], c.message[8 * j + 2], c.message[8 * j + 3],
                               c.message[8 * j + 4], c.message[8 * j + 5], c.message[8 * j + 6], c.message[8 * j + 7],
                               c.list[len(c.list) - 1].hash))
                 j=j+1
-                #line_pointer=lines
                 message_length=len(lines)
-    #print(lines)
-
-    #print(c.message[2])
-    #for j in range(0, len(c.message) // 8):
 
     with open('check_block.txt', mode='r') as file1:
         lines = file1.readlines()
@@ -219,7 +202,6 @@ while(1):
         if (check_block_length != len(lines)):
             file1.seek(check_block_length)
             for line in file1:
-                #line = line.strip()
                 line = line.strip('\n')
                 if(line.strip()):
                     c.show(int(line))
@@ -230,19 +212,7 @@ while(1):
         if (check_message_length != len(lines)):
             file2.seek(check_message_length)
             for line in file2:
-                # line = line.strip()
                 line = line.strip('\n')
                 if (line.strip()):
                     c.check_message(int(line))
             check_message_length = len(lines)
-     #   for line in file:
-      #      c.check_message(int(line))
-#for i i range(1,33):
-    #c.message_generator()
-
-
-#c.show(3)
-#c.isChainValid()
-#c.check_message(20)
-#json_res = json.dumps(tmine, sort_keys=True, indent=4, separators=(',', ': '))
-#print(json_res)
